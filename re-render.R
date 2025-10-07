deb6d1c1a007ddcf8bb520f14f1efceac3d6d833
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-10-07 11:39:19 -0700

    make re-render.R error less

--- re-render.R
+++ re-render.R
@@ -2,12 +2,17 @@ library(fs)
 library(quarto)
 
 # Deletes freeze folder
-dir_delete("_freeze")
-dir_delete("site_libs")
+
+if (dir_exists("_freeze")) {
+  dir_delete("_freeze")
+}
+if (dir_exists("site_libs")) {
+  dir_delete("site_libs")
+}
 
 # Deletes all `cache` folders
 dir_ls(recurse = TRUE, type = "directory", regexp = "cache") |>
-  dir_delete()
+  purrr::walk(dir_delete)
 
 # Update function lists
 source("make_function_lists.R")
