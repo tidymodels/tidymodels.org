6056c0c5be00068ee4567a90629d345f65653efb
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-03-10 12:52:49 -0700

    Replace pkgdown with direct Rd parsing in get_pkg_info()

--- make_function_lists/_utils.R
+++ make_function_lists/_utils.R
@@ -8,7 +8,7 @@ library(glue)
 library(utils)
 library(revdepcheck)
 library(fs)
-library(pkgdown)
+library(desc)
 library(urlchecker)
 library(stringr)
 library(readr)
@@ -28,8 +28,7 @@ get_available_packages <- function() {
 }
 
 # ------------------------------------------------------------------------------
-# Use the pkgdown package to parse the source files and put them into a usable
-# format
+# Parse source package Rd files directly (faster than pkgdown)
 
 get_pkg_info <- function(
     pkg,
@@ -38,41 +37,137 @@ get_pkg_info <- function(
     pattern = NULL,
     available = NULL
 ) {
-  src_file <-
-    download.packages(
-      pkg,
-      destdir = pth,
-      repos = "https://cran.rstudio.com/",
-      quiet = TRUE,
-      available = available
-    )
+  src_file <- download.packages(
+    pkg,
+    destdir = pth,
+    repos = "https://cran.rstudio.com/",
+    quiet = TRUE,
+    available = available
+  )
   if (nrow(src_file) != length(pkg)) {
-    return(NULL)
     rlang::warn(glue::glue("package {pkg} was not downloaded"))
+    return(NULL)
   }
 
   pkg_path <- fs::path(pth, pkg)
   on.exit(fs::dir_delete(pkg_path))
 
-  untar_res <- purrr::map_int(src_file[, 2], untar, exdir = pth)
+  untar(src_file[, 2], exdir = pth)
   fs::file_delete(src_file[, 2])
-  if (any(untar_res != 0)) {
-    rlang::abort(glue::glue("package {pkg} did not unpack correctly"))
+
+  # Get URLs from DESCRIPTION (always return as list for consistent column type)
+  desc_path <- fs::path(pkg_path, "DESCRIPTION")
+  pkg_urls <- if (fs::file_exists(desc_path)) {
+    d <- desc::desc(desc_path)
+    urls <- d$get_urls()
+    if (length(urls) == 0) list(NA_character_) else list(urls)
+  } else {
+    list(NA_character_)
   }
-  pkg_info <- pkgdown::as_pkgdown(pkg_path)
-  res <- pkg_info$topics
+
+  # Parse Rd files directly
+  man_path <- fs::path(pkg_path, "man")
+  if (!fs::dir_exists(man_path)) {
+    return(tibble::tibble(
+      package = pkg,
+      all_urls = pkg_urls,
+      file_out = character(),
+      functions = character(),
+      title = character()
+    ))
+  }
+
+  rd_files <- fs::dir_ls(man_path, glob = "*.Rd")
+
+  parse_rd_file <- function(rd_file) {
+    tryCatch({
+      rd <- tools::parse_Rd(rd_file)
+
+      get_tag <- function(rd, tag_name) {
+        for (el in rd) {
+          tag <- attr(el, "Rd_tag")
+          if (!is.null(tag) && tag == tag_name) {
+            return(paste(unlist(el), collapse = ""))
+          }
+        }
+        NA_character_
+      }
+
+      get_aliases <- function(rd) {
+        aliases <- c()
+        for (el in rd) {
+          tag <- attr(el, "Rd_tag")
+          if (!is.null(tag) && tag == "\\alias") {
+            aliases <- c(aliases, trimws(paste(unlist(el), collapse = "")))
+          }
+        }
+        aliases
+      }
+
+      is_internal <- function(rd) {
+        for (el in rd) {
+          tag <- attr(el, "Rd_tag")
+          if (!is.null(tag) && tag == "\\keyword") {
+            kw <- trimws(paste(unlist(el), collapse = ""))
+            if (kw == "internal") return(TRUE)
+          }
+        }
+        FALSE
+      }
+
+      name <- get_tag(rd, "\\name")
+      title <- get_tag(rd, "\\title")
+      aliases <- get_aliases(rd)
+      internal <- is_internal(rd)
+
+      if (length(aliases) == 0) aliases <- name
+
+      # Clean title: collapse newlines and multiple spaces
+      clean_title <- title |>
+        gsub("[\r\n]+", " ", x = _) |>
+        gsub("\\s+", " ", x = _) |>
+        trimws()
+
+      # Use Rd filename for file_out (pkgdown uses filename, not \name tag)
+      rd_basename <- fs::path_ext_remove(fs::path_file(rd_file))
+
+      tibble::tibble(
+        file_out = paste0(rd_basename, ".html"),
+        functions = aliases,
+        title = clean_title,
+        internal = internal
+      )
+    }, error = function(e) NULL)
+  }
+
+  res <- purrr::map(rd_files, parse_rd_file) |>
+    purrr::compact() |>
+    purrr::list_rbind()
+
+  if (nrow(res) == 0) {
+    return(tibble::tibble(
+      package = pkg,
+      all_urls = pkg_urls,
+      file_out = character(),
+      functions = character(),
+      title = character()
+    ))
+  }
+
   if (!keep_internal) {
     res <- dplyr::filter(res, !internal)
   }
-  res <-
-    res %>%
-    dplyr::select(file_out, functions = alias, title) %>%
-    tidyr::unnest(functions) %>%
-    dplyr::mutate(package = pkg, all_urls = list(pkg_info$desc$get_urls())) %>%
-    dplyr::relocate(package, all_urls)
+
+  res <- res |>
+    tidyr::unnest(functions) |>
+    dplyr::mutate(package = pkg, all_urls = pkg_urls) |>
+    dplyr::relocate(package, all_urls) |>
+    dplyr::select(-internal)
+
   if (!is.null(pattern)) {
     res <- dplyr::filter(res, grepl(pattern, functions))
   }
+
   res
 }
 
