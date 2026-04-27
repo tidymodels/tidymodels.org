23287a5cfc305ca4419b16c4cae397325f9e3ab7
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-04-27 08:27:21 -0700

    special case mixOmics install

--- R/install_packages.R
+++ R/install_packages.R
@@ -13,8 +13,18 @@ install_packages <- function(needed) {
   cli::cli_alert_info("{length(needed)} package{?s} required: {.pkg {needed}}")
 
   # catboost must be installed from GitHub
+  # mixOmics is a Bioconductor package and must be excluded from the main pak
+  # call to avoid a lazy-loading race where mixOmics tries to load ggplot2
+  # before it has finished installing.
   to_install <- ifelse(needed == "catboost", "catboost/catboost/catboost/R-package", needed)
+  to_install <- to_install[to_install != "mixOmics"]
   pak::pak(to_install, upgrade = TRUE)
+
+  # mixOmics must be installed after ggplot2 is fully in place
+  if ("mixOmics" %in% needed) {
+    cli::cli_alert_info("Installing mixOmics from Bioconductor...")
+    pak::pak("bioc::mixOmics", upgrade = TRUE)
+  }
 
   # torch must be installed explicitly for brulee to work
   if ("brulee" %in% needed) {
